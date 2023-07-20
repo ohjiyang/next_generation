91c8c91bea7ade583dbc6c9e9feb529354826662
ohjiyang <ohjiyung@icloud.com>
2023-07-20 11:26:54 +0600

    initial commit

---
+++ webpack.config.ts
@@ -0,0 +1,14 @@
+import path from 'path';
+import webpack from 'webpack';
+
+const config: webpack.Configuration = {
+  mode: 'production',
+  entry: './domains/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'index.js',
+    clean: true
+  },
+};
+
+export default config;
